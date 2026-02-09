19daf8a4036e356d3151dca70690a1f5dba7125f
Rados Mijatovic <rashke89@gmail.com>
2026-02-09 22:13:00 +0100

    build fail fix 01

--- pages/api/[...path].js
+++ pages/api/[...path].js
@@ -1,9 +1,9 @@
 const apiApp = require('../../server/apiApp');
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   // Ensure routes work under /api
   if (req.url.startsWith('/api/')) {
     req.url = req.url.replace('/api', '');
   }
   return apiApp(req, res);
-};
+}
